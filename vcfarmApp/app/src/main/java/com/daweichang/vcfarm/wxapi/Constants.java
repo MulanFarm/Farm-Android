15dab74894fe3ae3be85ebaf86ce6352e66a73b6
hanzhanbing <1655661337@qq.com>
2018-08-26 11:12:31 +0800

    添加微信秘钥

--- vcfarmApp/app/src/main/java/com/daweichang/vcfarm/wxapi/Constants.java
+++ vcfarmApp/app/src/main/java/com/daweichang/vcfarm/wxapi/Constants.java
@@ -3,6 +3,7 @@ package com.daweichang.vcfarm.wxapi;
 public class Constants {
     // APP_ID 替换为你的应用从官方网站申请到的合法appId
     public static final String APP_ID = "wx55bc4907e6c26338";//appid
+    public static final String APP_SECRET = "1932e376d6805320522ad16977ffddf8";//appsecret
     public static final String MCH_ID = "1443484002";//商户号
     //API密钥， 这个在商户平台里面配置 API安全里头
     public static final String API_KEY = "ff0959b7f0efe24e09e3af2857e34b7a";
